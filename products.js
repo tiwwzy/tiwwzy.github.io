ae7697c78af9659209fcfad9a929a3b2414a2ea5
toluwani <TI130@live.mdx.ac.uk>
2020-11-19 22:44:27 +0100

    added files

---
+++ products.js
@@ -0,0 +1,100 @@
+let products = [
+    {
+        
+        "id": 1020,
+        "title": "Mathematics",
+        "location": "Paris",
+        "price": 20,
+        "image": "images/mathematic.png",
+        "availableInventory": 5, //was 5
+        "rating": 2,
+        
+    },
+    {
+        "id": 1002,
+        "title": "English",
+        "location": "America",
+        "price": 30,
+        "image": "images/English.jpg",
+        "availableInventory": 5, //was 5
+        "rating": 4,
+    },
+    {
+        "id": 1003,
+        "title": "Economics",
+        "description": "5kg bag of <b> irrestible </b> cow food",
+        "location": "London",
+        "price": 40,
+        "image": "images/Economics.jpg",
+        "availableInventory": 5, //was 5
+        "rating": 5,
+    },
+    {
+        "id": 1004,
+        "title": "Biology",
+        "location": "Spain",
+        "price": 50,
+        "image": "images/Biology.jpg",
+        "availableInventory": 5, //was 5
+        "rating": 5,
+    },
+    {
+        "id": 1005,
+        "title": "Physic",
+        "location": "Malta",
+        "price": 60,
+        "image": "images/Physic.jpg",
+        "availableInventory": 5, //was 5
+        "rating": 5,
+    },
+    {
+        "id": 1006,
+        "title": "Chemistry",
+        "location": "Nigeria",
+        "price": 70,
+        "image": "images/Chemistry.jpg",
+        "availableInventory": 5, //was 5
+        "rating": 5,
+    },
+    {
+        "id": 1007,
+        "title": "History",
+        "location": "Ghana",
+        "price": 80,
+        "image": "images/History.jpg",
+        "availableInventory": 5, //was 5
+        "rating": 5,
+    },
+    {
+        "id": 1008,
+        "title": "Business study",
+        "location": "Iceland",
+        "price": 90,
+        "image": "images/Business study.jpg",
+        "availableInventory": 5, //was 5
+        "rating": 5,
+    },
+    {
+        "id": 1009,
+        "title": "Geography",
+        "location": "France",
+        "price": 100,
+        "image": "images/Geography.jpg",
+        "availableInventory": 5, //was 5
+        "rating": 5,
+    },
+    {
+        "id": 1010,
+        "title": "Statistic",
+        "location": "Argentina",
+        "price": 110,
+        "image": "images/Statistics.jpg",
+        "availableInventory": 5, //was 5
+        "rating": 5,
+    },
+    
+
+    
+
+
+]
